ef9ac2836642c187d90fec5f15886ac5bb951f8a
Samu K <samu.kaarlela@gmail.com>
2022-10-07 10:42:23 +0300

    Finished part6 (except 10,11)

--- H6/datadb100_h6_t13_150974219.sql
+++ H6/datadb100_h6_t13_150974219.sql
@@ -1,3 +1,8 @@
 -- Tietokantojen perusteet - Database basics
 -- H6 T13
 -- samu.kaarlela@tuni.fi
+
+SELECT artwork_name
+FROM artwork
+WHERE year_created BETWEEN 1500 AND 1600
+ORDER BY artwork_name ASC;
--- H6/datadb100_h6_t14_150974219.sql
+++ H6/datadb100_h6_t14_150974219.sql
@@ -1,3 +1,7 @@
 -- Tietokantojen perusteet - Database basics
 -- H6 T14
 -- samu.kaarlela@tuni.fi
+
+SELECT artwork_name, value*1.1 AS increased_value
+FROM artwork
+ORDER BY artwork_name
